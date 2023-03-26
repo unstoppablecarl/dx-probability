557a11f948211ec1b637ef4bbffa148541f7921b
carl olsen <unstoppablecarlolsen@gmail.com>
2023-03-26 15:46:34 -0400

    initial

--- src/lib/combat.js
+++ src/lib/combat.js
@@ -1,40 +1,46 @@
+
 export function combat(atkCombo, defCombo) {
 
     atkCombo = [].concat(atkCombo)
     defCombo = [].concat(defCombo)
 
-    let atk = atkCombo.sort().reverse()
-    let def = defCombo.sort().reverse()
+    // should already be sorted and reversed
+    let atk = atkCombo//.sort().reverse()
+    let def = defCombo//.sort().reverse()
 
-    let debug = {}
+    // let debug = {}
     let successes = atk.length
 
     let blocks = 0
 
-    atk.forEach((a) => {
-        debug[a] = 'success'
+    // atk.forEach((a) => {
+    //     debug[a] = 'success'
+    // })
 
-    })
+    const atkLen = atk.length
+    const defLen = def.length
+    let dIndex = 0
 
-    atk.forEach((a) => {
-        if (!def.length) {
-            return
+    for (let ai = 0; ai < atkLen; ai++) {
+        if (dIndex > defLen) {
+            break
         }
 
-        let d = def[0]
+        let a = atk[ai]
+        let d = def[dIndex]
 
         if (a <= d) {
-            def.shift()
-            debug[a] = 'blocked by: ' + d
+            dIndex++
+            // debug[a] = 'blocked by: ' + d
             successes--
             blocks++
         }
-    })
+    }
 
     return Object.freeze({
         successes,
         blocks,
-        debug,
+        // debug,
     })
 
 }
